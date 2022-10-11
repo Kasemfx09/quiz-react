72b85112a703036c3548541be299b456ab7ea612
Md Abul Kasem <akmanik15@gmail.com>
2022-10-11 17:22:11 +0600

    updated

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 
 
 const Chart = ({totalQuestion}) => {
     const {name, total} = totalQuestion;
-    const data = [{name: 'name', total: 'total'}];
+    const data = [{name: name, total: total,}];
 
+ 
+    return (   
+      <BarChart width={600} height={300} data={data}>
+      <XAxis dataKey="name" stroke="#8884d8" />
+      <YAxis />
+      <Tooltip />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <Bar dataKey="total" fill="#8884d8" barSize={30} />
+    </BarChart>
    
-    return (
-        <LineChart width={600} height={300} data={data}>
-    <Line type="monotone" dataKey="total" stroke="#8884d8" />
-    <CartesianGrid stroke="#ccc" />
-    <XAxis dataKey="name" />
-    <YAxis />
-  </LineChart>
     );
     
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -3,14 +3,15 @@ import { useLoaderData } from "react-router-dom";
 import Chart from "../Chart/Chart";
 
 const Statistics = () => {
-  const totalQuestions = useLoaderData();
-  // console.log(totalQuestions );
+  const totalQuestions = useLoaderData();  
   return (
     <div>
       <h2>This is a Statistics oage</h2>
         {
           totalQuestions.data.map(totalQuestion=><Chart key={totalQuestion.id} totalQuestion={totalQuestion}></Chart>)
         }
+
+        
     </div>
   );
 };
