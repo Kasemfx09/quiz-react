d6ff0b1150760220f095eaebb696cba092de740c
Md Abul Kasem <akmanik15@gmail.com>
2022-10-11 17:49:59 +0600

    Added chart

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import Chart from "../Chart/Chart";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+// import Chart from "../Chart/Chart";
 
 const Statistics = () => {
   const totalQuestions = useLoaderData();  
+  
+  
   return (
-    <div>
-      <h2>This is a Statistics oage</h2>
-        {
-          totalQuestions.data.map(totalQuestion=><Chart key={totalQuestion.id} totalQuestion={totalQuestion}></Chart>)
-        }
+    <div className="py-6">   
 
-        
+    <BarChart width={600} height={300} data={totalQuestions.data}>
+      <XAxis dataKey="name" stroke="#8884d8" />
+      <YAxis />
+      <Tooltip />
+      <CartesianGrid stroke="#bbb" strokeDasharray="5 5" />
+      <Bar dataKey="total" fill="#8884d8" barSize={30} />
+    </BarChart>
     </div>
   );
 };
